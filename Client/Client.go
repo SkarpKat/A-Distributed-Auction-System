2e4088b58c793d231411471e567511aa62cabb09
Oliver <oash@itu.dk>
2023-11-22 17:03:04 +0100

    Extra Changes

--- Client/Client.go
+++ Client/Client.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"sync"
 
 	Node "github.com/SkarpKat/A-Distributed-Auction-System/Server/proto"
 	"google.golang.org/grpc"
@@ -11,9 +15,29 @@ import (
 
 var (
 	clientName = flag.String("name", "client", "The name of the client")
-	clientID   = flag.Int("id", 0, "The ID of the client")
-	nodePorts  = []string{"8080", "8081", "8082", "8083", "8084", "8085", "8086", "8087", "8088", "8089"}
+	// clientID   = flag.Int("id", 0, "The ID of the client")
+	nodePorts = []string{"8080", "8081", "8082"}
+	joined    = false
 )
+
+type resultResponse struct {
+	bidder string
+	bid    int64
+	status string
+}
+
+type bidResponse struct {
+	bidder string
+	bid    int64
+	status string
+}
+
+type statusResponse struct {
+	bidder string
+	bid    int64
+	status string
+	winner bool
+}
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -24,12 +48,135 @@ func main() {
 	clientConnections := make([]Node.AuctionClient, len(nodePorts))
 
 	for i, port := range nodePorts {
-		conn, err := grpc.DialContext(ctx, "localhost:"+port, grpc.WithInsecure(), grpc.WithBlock())
+		conn, err := grpc.Dial("localhost:"+port, grpc.WithInsecure(), grpc.WithBlock())
 		if err != nil {
 			log.Printf("Failed to dial to node with port: %v", err)
 		}
 		defer conn.Close()
 		clientConnections[i] = Node.NewAuctionClient(conn)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		command := scanner.Text()
+		switch command {
+		case "bid":
+			fmt.Printf("Enter the amount you would like to bid: ")
+			bid := 0
+			// Read the bid from the user
+			_, err := fmt.Scanf("%d", &bid)
+			if err != nil {
+				log.Printf("Failed to read bid: %v", err)
+			}
+			responses := make([]bidResponse, len(clientConnections))
+
+			// Send the bid to all nodes
+			for _, client := range clientConnections {
+				rsp, err := client.Bid(ctx, &Node.BidRequest{Bidder: *clientName, Bid: int64(bid)})
+				if err != nil {
+					log.Printf("Failed to bid: %v", err)
+				}
+				log.Printf("Bid response: %v", rsp.Bidder)
+				responses = append(responses, bidResponse{bidder: rsp.Bidder, bid: rsp.Bid, status: rsp.Status})
+			}
+
+			// Check if all responses are the same and print the status
+			for _, rsp := range responses {
+				// Check if all responses are the same
+				if rsp.bidder != responses[0].bidder || rsp.bid != responses[0].bid || rsp.status != responses[0].status {
+					log.Printf("Error: Not all responses are the same")
+				}
+
+			}
+			if responses[0].bidder == *clientName {
+				fmt.Printf("You are winning with a bid of %d\n", responses[0].bid)
+			} else {
+				fmt.Printf("Your bid of %d could not competet with %d from %v\n", bid, responses[0].bid, responses[0].bidder)
+			}
+
+		case "status":
+			// Create a slice of status responses
+			responses := make([]statusResponse, len(clientConnections))
+
+			// Get the status from all nodes
+			for _, client := range clientConnections {
+				rsp, err := client.Status(ctx, &Node.StatusRequest{Bidder: *clientName})
+				if err != nil {
+					log.Printf("Failed to get status: %v", err)
+				}
+				responses = append(responses, statusResponse{bidder: rsp.Bidder, bid: rsp.Bid, status: rsp.Status, winner: rsp.Winner})
+			}
+
+			// Check if all responses are the same and print the status
+			for _, rsp := range responses {
+				// Check if all responses are the same
+
+				if rsp.bidder != responses[0].bidder || rsp.bid != responses[0].bid || rsp.status != responses[0].status {
+					log.Printf("Error: Not all responses are the same")
+				}
+			}
+
+			if responses[0].winner {
+				fmt.Printf("You are winning with a bid of %d\n", responses[0].bid)
+			} else {
+				fmt.Printf("You are not winning against a bid of %d\n", responses[0].bid)
+			}
+		case "join":
+			if joined {
+				fmt.Printf("You have already joined the auction\n")
+			} else {
+				joined = true
+				fmt.Printf("You have joined the auction\n")
+				go func() {
+					// Create wait group
+					wg := sync.WaitGroup{}
+
+					responses := make([]resultResponse, len(clientConnections))
+
+					// Create mutex
+					arbiter := sync.Mutex{}
+
+					for _, client := range clientConnections {
+						wg.Add(1)
+						go func(client Node.AuctionClient) {
+							rsp, err := client.Result(ctx, &Node.ResultRequest{Bidder: *clientName})
+							if err != nil {
+								log.Printf("Failed to get result: %v", err)
+							}
+							// Print the result of the auction
+							fmt.Printf("The winner is %s with a bid of %d\n", rsp.Bidder, rsp.Bid)
+							response := resultResponse{bidder: rsp.Bidder, bid: rsp.Bid, status: rsp.Status}
+							arbiter.Lock()
+							responses = append(responses, response)
+							arbiter.Unlock()
+							wg.Done()
+						}(client)
+
+					}
+					wg.Wait()
+
+					for _, rsp := range responses {
+						// Print all responses
+						fmt.Printf("Bidder: %s, Bid: %d, Status: %s\n", rsp.bidder, rsp.bid, rsp.status)
+
+						//Check if data is the same
+						if rsp.bidder != responses[0].bidder || rsp.bid != responses[0].bid || rsp.status != responses[0].status {
+							log.Printf("Error: Not all responses are the same")
+						}
+					}
+
+					fmt.Printf("The auction is over\n")
+					fmt.Printf("The winner is %s with a bid of %d\n", responses[1].bidder, responses[1].bid)
+
+				}()
+			}
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Printf("Invalid command\n Commands:\n bid - Bid on the auction\n status - Get the status of the auction\n Join - Join the auction\n")
+		}
+
 	}
 
 }
--- Server/Node/ServerNode.go
+++ Server/Node/ServerNode.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"time"
 
 	Node "github.com/SkarpKat/A-Distributed-Auction-System/Server/proto"
@@ -16,7 +18,7 @@ var (
 	nodePort = flag.String("port", "8080", "The port of the node")
 	isSlow   = flag.Bool("slow", false, "Whether the node is slow or not")
 	start    = false
-	duration = flag.Int("duration", 0, "The duration of the auction in seconds")
+	duration = flag.Int("duration", 60, "The duration of the auction in seconds")
 )
 
 type AuctionServer struct {
@@ -30,8 +32,10 @@ type AuctionServer struct {
 }
 
 func (s *AuctionServer) Bid(ctx context.Context, in *Node.BidRequest) (*Node.BidResponse, error) {
+	// Print the recieved bid and bidder
+	fmt.Printf("Bid recieved: %d from %s\n", in.Bid, in.Bidder)
 
-	if in.Bid > s.currentbid {
+	if in.Bid > s.currentbid && start {
 		s.currentbid = in.Bid
 		s.currentbidder = in.Bidder
 		return &Node.BidResponse{Bidder: s.currentbidder, Bid: s.currentbid, Status: s.status}, nil
@@ -41,6 +45,9 @@ func (s *AuctionServer) Bid(ctx context.Context, in *Node.BidRequest) (*Node.Bid
 }
 
 func (s *AuctionServer) Status(ctx context.Context, in *Node.StatusRequest) (*Node.StatusResponse, error) {
+	// Print the requester
+	fmt.Printf("Status requested by %s\n", in.Bidder)
+
 	if in.Bidder == s.currentbidder {
 		return &Node.StatusResponse{Bidder: s.currentbidder, Bid: s.currentbid, Status: s.status, Winner: true}, nil
 	} else {
@@ -49,6 +56,8 @@ func (s *AuctionServer) Status(ctx context.Context, in *Node.StatusRequest) (*No
 }
 
 func (s *AuctionServer) Result(ctx context.Context, in *Node.ResultRequest) (*Node.ResultResponse, error) {
+	// Print the requester who joined the auction
+	fmt.Printf("%s has joined the auction\n", in.Bidder)
 	start = true
 
 	if start {
@@ -58,22 +67,23 @@ func (s *AuctionServer) Result(ctx context.Context, in *Node.ResultRequest) (*No
 
 	start = false
 
+	// Print the result of the auction
+	fmt.Printf("The winner is %s with a bid of %d\n", s.currentbidder, s.currentbid)
+
 	return &Node.ResultResponse{Bidder: s.currentbidder, Bid: s.currentbid, Status: s.status}, nil
 }
 
 func main() {
-	// Define the address and port for the server node
-	address := "localhost"
 
 	flag.Parse()
 
 	// Create a TCP listener on the specified address and port
-	listener, err := net.Listen("tcp", address+":"+*nodePort)
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", *nodePort))
 	if err != nil {
 		log.Fatal("Error starting the server:", err)
 	}
 
-	fmt.Println("Server node started on", address+":"+*nodePort)
+	fmt.Println("Server node started on localhost:" + *nodePort)
 
 	grpcServer := grpc.NewServer()
 
@@ -83,8 +93,36 @@ func main() {
 
 	log.Printf("Server node listening on port %v", *nodePort)
 
+	go func() {
+		//Make commands to check values of currentbid, currentbidder, status
+		scanner := bufio.NewScanner(os.Stdin)
+
+		for scanner.Scan() {
+			command := scanner.Text()
+			switch command {
+			case "currentbid":
+				fmt.Printf("Current bid is: %d\n", AuctionServer.currentbid)
+			case "currentbidder":
+				fmt.Printf("Current bidder is: %s\n", AuctionServer.currentbidder)
+			case "status":
+				fmt.Printf("Status is: %s and start bool is %v\n", AuctionServer.status, start)
+			case "start":
+				fmt.Printf("Starting auction\n")
+				AuctionServer.status = "open"
+				start = true
+			case "stop":
+				fmt.Printf("Stopping auction\n")
+				AuctionServer.status = "closed"
+				start = false
+			case "shutdown":
+				os.Exit(0)
+			default:
+				fmt.Printf("Command not recognized\n Commands are: currentbid, currentbidder, status, shutdown\n")
+			}
+		}
+	}()
+
 	if err := grpcServer.Serve(listener); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
-
 }
